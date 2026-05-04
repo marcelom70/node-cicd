e3f0c67df430b243f0887495fc59fe2efc266dab
marcelom70 <marcelo.marcelom@gmail.com>
2026-05-04 20:13:51 -0300

    Actions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! This is a Node.js CI/CD application.');
+  res.send('<h1>Hello World! This is a Node.js CI/CD application.</h1>');
 });
 
 app.listen(port, () => {
